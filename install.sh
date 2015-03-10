d66649805b8731bc748bea347d4e69c1404503e5
David Pizarro <dpizarro89@gmail.com>
2015-03-10 22:12:23 +0100

    Added a new method to install templates (sh)

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Installs Android Studio Live Templates into your user configs.
+
+echo "Installing Android Studio Live Templates..."
+
+for i in $HOME/Library/Preferences/AndroidStudio*/templates \
+         $HOME/.AndroidStudio*/config/templates
+do
+  cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/templates/* $i 2> /dev/null
+done
+
+echo "Done."
+echo ""
+echo "Restart AndroidStudio and enjoy!"
